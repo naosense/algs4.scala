58a6e2dafa71a4c6388e49a6fa76613d9dc901fc
pingao <liupingao@jd.com>
2020-07-29 17:39:40 +0800

    util

--- src/main/scala/sort/Insertion.scala
+++ src/main/scala/sort/Insertion.scala
@@ -3,14 +3,8 @@ package sort
 import scala.util.Random
 
 object Insertion {
-  def sort[A](a: Array[A])(implicit ord: A => Ordered[A]): Unit = {
-    val n = a.length
-    for (i <- 1 until n; j <- i until 0 by -1) {
-      if (a(j) < a(j - 1)) exch(a, j, j - 1)
-    }
-  }
 
-  def sorted[A: Ordering](a: Array[A]): Unit = {
+  def sort[A: Ordering](a: Array[A]): Unit = {
     val n = a.length
     for (i <- 1 until n; j <- i until 0 by -1) {
       if (less(a(j), a(j - 1))) exch(a, j, j - 1)
@@ -22,7 +16,7 @@ object Insertion {
     show(a, false)
     sort(a)
     show(a)
-    sorted(a)((x, y) => y - x)
+    sort(a)((x, y) => y - x)
     show(a)
   }
 }
